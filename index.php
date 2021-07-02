0e359f2df1b28ea0dbe0902347374cdc52807d46
shuhei05 <qoo0228y@gmail.com>
2021-07-02 15:27:48 +0900

    update:index

--- index.php
+++ index.php
@@ -3,6 +3,27 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World</p>';
+
+      $a = "PHPの基礎";
+      $a ="がんばりましょう";
+
+      print $a;
+
+      define("TITLE","PHPの基礎");
+
+      define("TITLE","タイトル"); //再定義することはできない。
+
+      define("TAX",1.05);
+      print 300 * TAX;
+
+      $a = "PHPの基礎";
+
+      print '1. タイトルは $a です。<br />';
+      print "2. タイトルは $a です。<br />";
+      print "3. タイトルは$aです。<br />";
+      print "4. タイトルは{$a}です。<br />";
+      print "5. タイトルは".$a."です。<br />";
+ ?>
  </body>
 </html>
