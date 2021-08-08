8ed075c9d06a29fe13a4de228b03fcf0cb560152
shuhei05 <qoo0228y@gmail.com>
2021-08-08 23:26:13 +0900

    add:file

---
+++ fizzbuzz.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+ 
+for ($i = 1; $i <= 100; $i++) {
+    if (($i % 3 === 0) && ($i % 5 === 0)) {
+        echo 'FizzBuzz';
+    } elseif ($i % 3 === 0) {
+        echo 'Fizz';
+    } elseif ($i % 5 === 0) {
+        echo 'Buzz';
+    } else {
+        echo $i;
+    }
+ 
+    echo PHP_EOL;
+}
